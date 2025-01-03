b4916a012157d9805c469365adbdc2c8f897b337
bs-koo <bs.koo@sqisoft.com>
2025-01-03 15:19:47 +0900

    feat: Member 추가 - #2

---
+++ src/main/java/org/example/odiya/member/domain/Member.java
@@ -0,0 +1,39 @@
+package org.example.odiya.member.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.odiya.common.domain.BaseEntity;
+import org.example.odiya.mate.domain.Mate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String inviteCode;
+
+    @Embedded
+    private DeviceToken deviceToken;
+
+    @OneToMany(mappedBy = "member")
+    private List<Mate> mates = new ArrayList<>();
+}
