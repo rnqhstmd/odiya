0ff078d378f6adf3cc8e7ae85e7152709a3ea50d
BonSeung <bs.koo@sqisoft.com>
2025-01-05 17:27:42 +0900

    feat: 인증되지 않은 사용자들의 리소스 접근 시 처리를 담당하는 핸들러 추가 - #6

---
+++ src/main/java/org/example/odiya/security/auth/handler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,43 @@
+package org.example.odiya.security.auth.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.odiya.common.exception.ApiException;
+import org.example.odiya.common.exception.UnauthorizedException;
+import org.example.odiya.common.exception.type.ErrorType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static org.example.odiya.common.exception.type.ErrorType.TOKEN_NOT_INCLUDED_ERROR;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        if (authException.getCause() instanceof ApiException apiException) {
+            setResponse(response, apiException);
+            return;
+        }
+
+        ApiException apiException = new UnauthorizedException(TOKEN_NOT_INCLUDED_ERROR);
+        setResponse(response, apiException);
+    }
+
+    private void setResponse(HttpServletResponse response, ApiException apiException) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        ErrorType errorType = apiException.getErrorType();
+        int status = apiException.getHttpStatus().value();
+        response.setStatus(status);
+
+        response.getWriter().println(
+                "{\"status\" : \"" + status + "\"," +
+                        "\"errorCode\" : \"" + errorType.getErrorCode() + "\"," +
+                        "\"message\" : \"" + errorType.getMessage() + "\"}");
+    }
+}
