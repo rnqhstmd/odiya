d7d4af1f5366f0900b8f0b201d6b27f60fac114c
bs-koo <bs.koo@sqisoft.com>
2025-01-06 17:34:02 +0900

    feat: 테스트용 - #15

--- src/test/java/org/example/odiya/OdiyaApplicationTests.java
+++ src/test/java/org/example/odiya/OdiyaApplicationTests.java
@@ -3,13 +3,14 @@ package org.example.odiya;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class OdiyaApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception {
     }
 
 }
