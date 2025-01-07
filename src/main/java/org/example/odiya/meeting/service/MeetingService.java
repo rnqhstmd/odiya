a54686cdac81d657c6ab24ed0300a013a8a5b159
bs-koo <bs.koo@sqisoft.com>
2025-01-07 17:53:12 +0900

    feat: 약속 서비스 추가 - #20

---
+++ src/main/java/org/example/odiya/meeting/service/MeetingService.java
@@ -0,0 +1,8 @@
+package org.example.odiya.meeting.service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MeetingService {
+}
