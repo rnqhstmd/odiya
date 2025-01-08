3216cfe3ad76c4bd73ca6447bb32fb80fcfe0c90
bs-koo <bs.koo@sqisoft.com>
2025-01-08 15:14:43 +0900

    feat: 장소 상세 조회 API 추가 - #19

--- src/main/java/org/example/odiya/map/controller/MapController.java
+++ src/main/java/org/example/odiya/map/controller/MapController.java
@@ -1,12 +1,15 @@
 package org.example.odiya.map.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.odiya.map.dto.response.MapSearchResponse;
 import org.example.odiya.map.service.MapService;
+import org.example.odiya.map.dto.response.PlaceSearchResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Tag(name = "Map API", description = "카카오맵 장소 관련 API")
 @RestController
 @RequestMapping("/api/maps")
 @RequiredArgsConstructor
@@ -14,8 +17,15 @@ public class MapController {
 
     private final MapService mapService;
 
-    @GetMapping("/search")
+    @Operation(summary = "장소 전체 조회 API", description = "사용자가 키워드로 장소를 검색합니다.")
+    @GetMapping("/places")
     public ResponseEntity<MapSearchResponse> searchLocation(@RequestParam String query) {
         return ResponseEntity.ok(mapService.searchByKeyword(query));
+    }
+
+    @Operation(summary = "장소 상세 조회 API", description = "사용자가 선택한 장소의 상세 정보를 조회합니다.")
+    @GetMapping("/places/{placeId}")
+    public ResponseEntity<PlaceSearchResponse.PlaceDto> getSelectedPlace(@PathVariable String placeId) {
+        return ResponseEntity.ok(mapService.findPlaceById(placeId).toPlaceDto());
     }
 }
