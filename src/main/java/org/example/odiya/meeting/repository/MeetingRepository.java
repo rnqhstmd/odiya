b5b376cf8b4b9aa246e16c2c1d6c4d28b5009804
bs-koo <bs.koo@sqisoft.com>
2025-01-08 13:28:47 +0900

    feat: inviteCode로 Meeting 객체 찾는 메서드 추가 - #19

--- src/main/java/org/example/odiya/meeting/repository/MeetingRepository.java
+++ src/main/java/org/example/odiya/meeting/repository/MeetingRepository.java
@@ -3,5 +3,9 @@ package org.example.odiya.meeting.repository;
 import org.example.odiya.meeting.domain.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+
+    Optional<Meeting> findByInviteCode(String inviteCode);
 }
---
+++ src/main/java/org/example/odiya/meeting/service/MeetingQueryService.java
@@ -0,0 +1,23 @@
+package org.example.odiya.meeting.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.odiya.common.exception.NotFoundException;
+import org.example.odiya.meeting.domain.Meeting;
+import org.example.odiya.meeting.repository.MeetingRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.example.odiya.common.exception.type.ErrorType.MEETING_NOT_FOUND_ERROR;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MeetingQueryService {
+
+    private final MeetingRepository meetingRepository;
+
+    public Meeting findMeetingByInviteCode(String inviteCode) {
+        return meetingRepository.findByInviteCode(inviteCode)
+                .orElseThrow(() -> new NotFoundException(MEETING_NOT_FOUND_ERROR));
+    }
+}
