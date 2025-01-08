398e1dfcd3abef998dc10699513e7d140bf1bfa5
bs-koo <bs.koo@sqisoft.com>
2025-01-08 13:26:46 +0900

    feat: MateQueryService 참가되어있는 약속 mate 검증 메서드 추가 - #19

---
+++ src/main/java/org/example/odiya/mate/service/MateQueryService.java
@@ -0,0 +1,23 @@
+package org.example.odiya.mate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.odiya.common.exception.ConflictException;
+import org.example.odiya.mate.repository.MateRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.example.odiya.common.exception.type.ErrorType.DUPLICATION_MATE_ERROR;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MateQueryService {
+
+    private final MateRepository mateRepository;
+
+    public void isMateExist(Long memberId, Long meetingId) {
+        if (mateRepository.existsByMemberIdAndMeetingId(memberId, meetingId)) {
+            throw new ConflictException(DUPLICATION_MATE_ERROR);
+        }
+    }
+}
