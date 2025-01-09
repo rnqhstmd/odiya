b847b7c39f78ca30c323ccac90fa3c9ab780bf9f
bs-koo <bs.koo@sqisoft.com>
2025-01-09 16:13:42 +0900

    feat: 스케쥴링 설정 추가 - #30

---
+++ src/main/java/org/example/odiya/common/config/SchedulerConfig.java
@@ -0,0 +1,25 @@
+package org.example.odiya.common.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);  // 동시 실행 가능한 스케줄 작업 수
+        scheduler.setThreadNamePrefix("scheduled-task-");
+        scheduler.setErrorHandler(throwable ->
+                log.error("스케쥴러 작업 에러: ", throwable));
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        return scheduler;
+    }
+}
