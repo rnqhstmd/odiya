b8cc024843729615599dbc70bf444b5813908172
bs-koo <bs.koo@sqisoft.com>
2025-01-09 16:25:51 +0900

    fix: @SpringBootTest 어노테이션 제거 후 MockitoExtension 사용 - #30

--- src/test/java/org/example/odiya/meeting/utils/MeetingOverDueSchedulerTest.java
+++ src/test/java/org/example/odiya/meeting/utils/MeetingOverDueSchedulerTest.java
@@ -3,9 +3,10 @@ package org.example.odiya.meeting.utils;
 import org.example.odiya.meeting.service.MeetingQueryService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.scheduling.support.SimpleTriggerContext;
 
@@ -18,13 +19,13 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class MeetingOverDueSchedulerTest {
 
-    @MockBean
+    @Mock
     private MeetingQueryService meetingQueryService;
 
-    @Autowired
+    @InjectMocks
     private MeetingOverDueScheduler scheduler;
 
     @Test
