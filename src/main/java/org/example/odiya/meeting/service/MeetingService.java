7a076d9c8036b0886dbdc44dae96154b7b0bc1a7
bs-koo <bs.koo@sqisoft.com>
2025-01-09 14:03:52 +0900

    feat: 종료된 약속인지 확인하는 검증코드 추가 - #28

--- src/main/java/org/example/odiya/meeting/service/MeetingService.java
+++ src/main/java/org/example/odiya/meeting/service/MeetingService.java
@@ -1,6 +1,7 @@
 package org.example.odiya.meeting.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.odiya.common.exception.BadRequestException;
 import org.example.odiya.mate.domain.Mate;
 import org.example.odiya.mate.service.MateQueryService;
 import org.example.odiya.mate.service.MateService;
@@ -13,6 +14,8 @@ import org.example.odiya.meeting.repository.MeetingRepository;
 import org.example.odiya.member.domain.Member;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.example.odiya.common.exception.type.ErrorType.MEETING_OVERDUE_ERROR;
 
 @Service
 @Transactional
@@ -51,6 +54,11 @@ public class MeetingService {
     public void joinMeeting(Member member, String inviteCode) {
         // inviteCode 로 약속 찾기
         Meeting meeting = meetingQueryService.findMeetingByInviteCode(inviteCode);
+
+        // 종료된 약속인지 확인
+        if (meeting.isOverdue()) {
+            throw new BadRequestException(MEETING_OVERDUE_ERROR);
+        }
 
         // 이미 참가한 사용자인지 확인
         mateQueryService.isMateExist(member.getId(), meeting.getId());
