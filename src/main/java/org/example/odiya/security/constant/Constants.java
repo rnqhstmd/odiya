9c8417d0d3c08eed2550a371b6cac0272710bb76
bs-koo <bs.koo@sqisoft.com>
2025-01-09 11:26:11 +0900

    refactor: 검증하지 않는 url 상수로 추가 - #26

---
+++ src/main/java/org/example/odiya/security/constant/Constants.java
@@ -0,0 +1,19 @@
+package org.example.odiya.security.constant;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String[] WHITE_LIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/webjars/**",
+            "/favicon.ico",
+            "/h2-console/**",
+            "/api/auth/**"
+    };
+}
