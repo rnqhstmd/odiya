faa25bcc1ed69cf82154b4f0f03007de31314b0e
bs-koo <bs.koo@sqisoft.com>
2025-01-13 17:14:21 +0900

    feat: placeName 제거 - #37

--- src/main/java/org/example/odiya/meeting/service/MeetingService.java
+++ src/main/java/org/example/odiya/meeting/service/MeetingService.java
@@ -1,9 +1,7 @@
 package org.example.odiya.meeting.service;
 
 import lombok.RequiredArgsConstructor;
-import org.example.odiya.common.exception.BadRequestException;
 import org.example.odiya.mate.domain.Mate;
-import org.example.odiya.mate.service.MateQueryService;
 import org.example.odiya.mate.service.MateService;
 import org.example.odiya.meeting.domain.Coordinates;
 import org.example.odiya.meeting.domain.Location;
@@ -15,8 +13,6 @@ import org.example.odiya.member.domain.Member;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.example.odiya.common.exception.type.ErrorType.MEETING_OVERDUE_ERROR;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -24,12 +20,9 @@ public class MeetingService {
 
     private final MeetingRepository meetingRepository;
     private final MateService mateService;
-    private final MateQueryService mateQueryService;
-    private final MeetingQueryService meetingQueryService;
 
     public MeetingResponse createMeeting(Member member, MeetingCreateRequest request) {
         Location location = new Location(
-                request.getPlaceName(),
                 request.getAddress(),
                 new Coordinates(request.getLatitude(), request.getLongitude())
         );
@@ -49,25 +42,5 @@ public class MeetingService {
         mateService.saveMate(mate);
 
         return MeetingResponse.from(savedMeeting);
-    }
-
-    public void joinMeeting(Member member, String inviteCode) {
-        // inviteCode 로 약속 찾기
-        Meeting meeting = meetingQueryService.findMeetingByInviteCode(inviteCode);
-
-        // 종료된 약속인지 확인
-        if (meeting.isOverdue()) {
-            throw new BadRequestException(MEETING_OVERDUE_ERROR);
-        }
-
-        // 이미 참가한 사용자인지 확인
-        mateQueryService.isMateExist(member.getId(), meeting.getId());
-
-        Mate mate = Mate.builder()
-                .member(member)
-                .meeting(meeting)
-                .build();
-
-        mateService.saveMate(mate);
     }
 }
