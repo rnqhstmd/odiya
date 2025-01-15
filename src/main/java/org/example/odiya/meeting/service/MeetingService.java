ec70f81418397536c7dd0ad758b4ca115c2bc16e
bs-koo <bs.koo@sqisoft.com>
2025-01-15 12:52:07 +0900

    feat: 약속 조회 기능 추가 - #27

--- src/main/java/org/example/odiya/meeting/service/MeetingService.java
+++ src/main/java/org/example/odiya/meeting/service/MeetingService.java
@@ -2,16 +2,21 @@ package org.example.odiya.meeting.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.odiya.mate.domain.Mate;
+import org.example.odiya.mate.service.MateQueryService;
 import org.example.odiya.mate.service.MateService;
 import org.example.odiya.meeting.domain.Coordinates;
 import org.example.odiya.meeting.domain.Location;
 import org.example.odiya.meeting.domain.Meeting;
 import org.example.odiya.meeting.dto.request.MeetingCreateRequest;
 import org.example.odiya.meeting.dto.response.MeetingCreateResponse;
+import org.example.odiya.meeting.dto.response.MeetingDetailResponse;
+import org.example.odiya.meeting.dto.response.MeetingListResponse;
 import org.example.odiya.meeting.repository.MeetingRepository;
 import org.example.odiya.member.domain.Member;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -19,7 +24,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class MeetingService {
 
     private final MeetingRepository meetingRepository;
+    private final MeetingQueryService meetingQueryService;
     private final MateService mateService;
+    private final MateQueryService mateQueryService;
 
     public MeetingCreateResponse createMeeting(Member member, MeetingCreateRequest request) {
         Location location = new Location(
@@ -42,5 +49,17 @@ public class MeetingService {
         mateService.saveMate(mate);
 
         return MeetingCreateResponse.from(savedMeeting);
+    }
+
+    public MeetingListResponse getMyMeetingList(Member member) {
+        int mateCount = mateQueryService.countByMeetingId(member.getId());
+        List<Meeting> meetingList = meetingQueryService.findOverdueMeetings(member.getId());
+        return MeetingListResponse.of(meetingList,mateCount);
+    }
+
+    public MeetingDetailResponse getMeetingDetail(Member member, Long meetingId) {
+        Meeting meeting = meetingQueryService.findMeetingsByMemberId(meetingId);
+        mateQueryService.validateMateExists(member.getId(), meetingId);
+        return MeetingDetailResponse.from(meeting);
     }
 }
