817ffbede1379affea0c07a143fdb646a5eed0ca
bs-koo <bs.koo@sqisoft.com>
2025-01-15 12:49:57 +0900

    feat: 약속에 속한 Mate Count 메서드 추가 - #27

--- src/main/java/org/example/odiya/mate/service/MateQueryService.java
+++ src/main/java/org/example/odiya/mate/service/MateQueryService.java
@@ -2,11 +2,13 @@ package org.example.odiya.mate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.odiya.common.exception.ConflictException;
+import org.example.odiya.common.exception.NotFoundException;
 import org.example.odiya.mate.repository.MateRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.example.odiya.common.exception.type.ErrorType.DUPLICATION_MATE_ERROR;
+import static org.example.odiya.common.exception.type.ErrorType.NOT_PARTICIPATED_MATE_ERROR;
 
 @Service
 @Transactional(readOnly = true)
@@ -19,5 +21,15 @@ public class MateQueryService {
         if (mateRepository.existsByMemberIdAndMeetingId(memberId, meetingId)) {
             throw new ConflictException(DUPLICATION_MATE_ERROR);
         }
+    }
+
+    public void validateMateExists(Long memberId, Long meetingId) {
+        if (!mateRepository.existsByMemberIdAndMeetingId(memberId, meetingId)) {
+            throw new NotFoundException(NOT_PARTICIPATED_MATE_ERROR);
+        }
+    }
+
+    public int countByMeetingId(Long meetingId) {
+        return mateRepository.countByMeetingId(meetingId);
     }
 }
