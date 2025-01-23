42875db6ee5f315127b3114ce26d182209536a95
bs-koo <bs.koo@sqisoft.com>
2025-01-23 11:30:54 +0900

    feat: mateId로 해당 eta 조회 메서드 추가 추가 - #42

--- src/main/java/org/example/odiya/eta/service/EtaQueryService.java
+++ src/main/java/org/example/odiya/eta/service/EtaQueryService.java
@@ -1,9 +1,13 @@
 package org.example.odiya.eta.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.odiya.common.exception.NotFoundException;
+import org.example.odiya.eta.domain.Eta;
 import org.example.odiya.eta.repository.EtaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.example.odiya.common.exception.type.ErrorType.MATE_ETA_NOT_FOUND_ERROR;
 
 @Service
 @Transactional(readOnly = true)
@@ -12,4 +16,8 @@ public class EtaQueryService {
 
     private final EtaRepository etaRepository;
 
+    public Eta findByMateId(Long mateId) {
+        return etaRepository.findByMateId(mateId)
+                .orElseThrow(() -> new NotFoundException(MATE_ETA_NOT_FOUND_ERROR));
+    }
 }
