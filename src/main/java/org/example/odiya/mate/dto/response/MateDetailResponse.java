e9ac530c16151cf42be13e43ee71e931f9f2202f
bs-koo <bs.koo@sqisoft.com>
2025-01-23 17:18:13 +0900

    feat: 참여자 이름, 예상 도착시간 필드 추가 - #43

--- src/main/java/org/example/odiya/mate/dto/response/MateDetailResponse.java
+++ src/main/java/org/example/odiya/mate/dto/response/MateDetailResponse.java
@@ -12,11 +12,19 @@ public class MateDetailResponse {
     @Schema(description = "참여자 ID", example = "1")
     private Long id;
 
-    @Schema(description = "참여자 출발지주소", example = "서울 강남구 테헤란로 411")
+    @Schema(description = "참가자 이름", example = "구본승")
+    private String name;
+
+    @Schema(description = "참여자 출발지주소", example = "서울 송파구 송파대로 345")
     private String originAddress;
+
+    @Schema(description = "예상 도착시간", example = "30")
+    private Long remainingMinutes;
 
     public MateDetailResponse(Mate mate) {
         this.id = mate.getId();
+        this.name = mate.getMember().getName();
         this.originAddress = mate.getOrigin().getAddress();
+        this.remainingMinutes = mate.getEstimatedTime();
     }
 }
