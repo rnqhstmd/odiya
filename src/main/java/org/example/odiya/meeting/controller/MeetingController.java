473ee2e6428c2240ed6e719982f7f9c2d76318cd
BonSeung <rnqhstmd9134@naver.com>
2025-02-01 23:16:23 +0900

    feat: 해당 약속 eta 업데이트 api 추가 - #44

--- src/main/java/org/example/odiya/meeting/controller/MeetingController.java
+++ src/main/java/org/example/odiya/meeting/controller/MeetingController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.odiya.common.annotation.AuthMember;
+import org.example.odiya.eta.service.EtaService;
 import org.example.odiya.meeting.dto.request.MeetingCreateRequest;
 import org.example.odiya.meeting.dto.response.MeetingCreateResponse;
 import org.example.odiya.meeting.dto.response.MeetingDetailResponse;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 public class MeetingController {
 
     private final MeetingService meetingService;
+    private final EtaService etaService;
 
     @Operation(summary = "약속 생성 API", description = "사용자가 약속을 생성합니다.")
     @PostMapping
@@ -50,5 +52,16 @@ public class MeetingController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(meetingDetail);
+    }
+
+    @Operation(summary = "참여자 도착 예정 시간 업데이트 API", description = "해당 약속의 모든 참여자들의 도착 예정 시간을 업데이트합니다.")
+    @PutMapping("/{id}/eta")
+    public ResponseEntity<Void> updateMeetingEta(
+            @AuthMember Member member,
+            @PathVariable("id") Long meetingId) {
+        etaService.updateEtaOfMate(meetingId, member.getId());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
     }
 }
