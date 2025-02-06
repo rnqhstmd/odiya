15606d7c085159a7ccf77138f07d80cb1c4da75c
bs-koo <bs.koo@sqisoft.com>
2025-02-06 14:15:39 +0900

    feat: fixtureGenerator 등록 설정 클래스 FixtureGeneratorConfig 추가 - #53

---
+++ src/test/java/org/example/odiya/common/config/FixtureGeneratorConfig.java
@@ -0,0 +1,30 @@
+package org.example.odiya.common.config;
+
+import org.example.odiya.common.Fixture.FixtureGenerator;
+import org.example.odiya.eta.repository.EtaRepository;
+import org.example.odiya.mate.repository.MateRepository;
+import org.example.odiya.meeting.repository.MeetingRepository;
+import org.example.odiya.member.repository.MemberRepository;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@TestConfiguration
+public class FixtureGeneratorConfig {
+
+    @Bean
+    public FixtureGenerator fixtureGenerator(
+            MemberRepository memberRepository,
+            MeetingRepository meetingRepository,
+            MateRepository mateRepository,
+            EtaRepository etaRepository
+    ) {
+        return new FixtureGenerator(
+                memberRepository,
+                meetingRepository,
+                mateRepository,
+                etaRepository
+        );
+    }
+}
