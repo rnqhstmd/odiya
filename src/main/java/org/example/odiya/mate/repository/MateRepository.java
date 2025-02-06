070af35e9fe86faff498df0b709b8297ee201343
bs-koo <bs.koo@sqisoft.com>
2025-02-06 14:19:37 +0900

    feat: 테스트용 메서드 추가 - #53

--- src/main/java/org/example/odiya/mate/repository/MateRepository.java
+++ src/main/java/org/example/odiya/mate/repository/MateRepository.java
@@ -3,9 +3,13 @@ package org.example.odiya.mate.repository;
 import org.example.odiya.mate.domain.Mate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MateRepository extends JpaRepository<Mate, Long> {
 
     boolean existsByMemberIdAndMeetingId(Long memberId, Long meetingId);
 
     int countByMeetingId(Long meetingId);
+
+    Optional<Mate> findByMemberIdAndMeetingId(Long memberId, Long meetingId);
 }
