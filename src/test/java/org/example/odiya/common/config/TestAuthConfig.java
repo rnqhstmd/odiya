660681292fc5cc74e883f5a23dde6c9b404913d9
bs-koo <bs.koo@sqisoft.com>
2025-02-06 15:19:55 +0900

    feat: Auth 설정 클래스 TestAuthConfig - #53

---
+++ src/test/java/org/example/odiya/common/config/TestAuthConfig.java
@@ -0,0 +1,22 @@
+package org.example.odiya.common.config;
+
+import org.example.odiya.common.Fixture.TokenFixture;
+import org.example.odiya.member.service.MemberQueryService;
+import org.example.odiya.security.jwt.provider.JwtProvider;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@Profile("test")
+@TestConfiguration
+public class TestAuthConfig {
+
+    @Bean
+    public JwtProvider jwtProvider(MemberQueryService memberQueryService) {
+        JwtProvider provider = new JwtProvider(memberQueryService);
+        ReflectionTestUtils.setField(provider, "key", TokenFixture.TEST_SECRET_KEY);
+        ReflectionTestUtils.setField(provider, "validityInMilliseconds", TokenFixture.VALID_EXPIRE_LENGTH);
+        return provider;
+    }
+}
