9b837cdb93d84ec983b8088e72422a7f9f9dd64a
bs-koo <bs.koo@sqisoft.com>
2025-02-06 16:24:56 +0900

    feat: TestAuthConfig 임포트 추가 - #53

--- src/test/java/org/example/odiya/common/BaseTest/BaseServiceTest.java
+++ src/test/java/org/example/odiya/common/BaseTest/BaseServiceTest.java
@@ -3,6 +3,7 @@ package org.example.odiya.common.BaseTest;
 import org.example.odiya.common.Fixture.DtoGenerator;
 import org.example.odiya.common.Fixture.FixtureGenerator;
 import org.example.odiya.common.config.FixtureGeneratorConfig;
+import org.example.odiya.common.config.TestAuthConfig;
 import org.example.odiya.common.config.TestRouteConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.event.RecordApplicationEvents;
 
-@Import({TestRouteConfig.class, FixtureGeneratorConfig.class})
+@Import({TestRouteConfig.class, FixtureGeneratorConfig.class, TestAuthConfig.class})
 @ActiveProfiles("test")
 @RecordApplicationEvents
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
