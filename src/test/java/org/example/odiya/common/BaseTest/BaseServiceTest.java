d10fc6d7a58ecada8eb7fc0c56c51aa8a32160cf
bs-koo <bs.koo@sqisoft.com>
2025-02-06 14:17:23 +0900

    feat: 각 테스트 서비스클래스를 위한 추상화 클래스 BaseServiceTest 추가 - #53

---
+++ src/test/java/org/example/odiya/common/BaseTest/BaseServiceTest.java
@@ -0,0 +1,23 @@
+package org.example.odiya.common.BaseTest;
+
+import org.example.odiya.common.Fixture.DtoGenerator;
+import org.example.odiya.common.Fixture.FixtureGenerator;
+import org.example.odiya.common.config.FixtureGeneratorConfig;
+import org.example.odiya.common.config.TestRouteConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.event.RecordApplicationEvents;
+
+@Import({TestRouteConfig.class, FixtureGeneratorConfig.class})
+@ActiveProfiles("test")
+@RecordApplicationEvents
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public abstract class BaseServiceTest {
+
+    @Autowired
+    protected FixtureGenerator fixtureGenerator;
+
+    protected DtoGenerator dtoGenerator = new DtoGenerator();
+}
