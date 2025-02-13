7f3435b55fd01b1722352b6d4ff95f1729f52a7f
bs-koo <bs.koo@sqisoft.com>
2025-02-13 17:46:49 +0900

    feat : Mate 생성 시 약속 토픽 구독, 참가알림, 리마인더 스케쥴링 로직 추가 - #57

--- src/main/java/org/example/odiya/mate/service/MateService.java
+++ src/main/java/org/example/odiya/mate/service/MateService.java
@@ -2,8 +2,10 @@ package org.example.odiya.mate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.odiya.common.exception.BadRequestException;
+import org.example.odiya.eta.domain.EtaStatus;
 import org.example.odiya.eta.service.EtaService;
 import org.example.odiya.mate.domain.Mate;
+import org.example.odiya.mate.dto.request.HurryUpRequest;
 import org.example.odiya.mate.dto.request.MateJoinRequest;
 import org.example.odiya.mate.dto.response.MateJoinResponse;
 import org.example.odiya.mate.repository.MateRepository;
@@ -12,11 +14,16 @@ import org.example.odiya.meeting.domain.Location;
 import org.example.odiya.meeting.domain.Meeting;
 import org.example.odiya.meeting.service.MeetingQueryService;
 import org.example.odiya.member.domain.Member;
+import org.example.odiya.notification.domain.FcmTopic;
+import org.example.odiya.notification.domain.types.EntryNotification;
+import org.example.odiya.notification.domain.types.HurryUpNotification;
+import org.example.odiya.notification.domain.types.ReminderNotification;
+import org.example.odiya.notification.service.NotificationService;
 import org.example.odiya.route.service.RouteService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.example.odiya.common.exception.type.ErrorType.MEETING_OVERDUE_ERROR;
+import static org.example.odiya.common.exception.type.ErrorType.*;
 
 @Service
 @Transactional
@@ -28,6 +35,7 @@ public class MateService {
     private final MeetingQueryService meetingQueryService;
     private final RouteService routeService;
     private final EtaService etaService;
+    private final NotificationService notificationService;
 
     public MateJoinResponse joinMeeting(Member member, MateJoinRequest request) {
         // inviteCode 로 약속 찾기
@@ -70,5 +78,48 @@ public class MateService {
 
         Mate savedMate = mateRepository.save(mate);
         etaService.saveFirstEtaOfMate(savedMate, estimatedTime);
+
+        FcmTopic fcmTopic = new FcmTopic(meeting);
+
+        // 약속 토픽 구독
+        notificationService.subscribeTopic(member.getDeviceToken(), fcmTopic);
+
+        // 약속 참가 알림 스케줄링
+        EntryNotification entryNotification = new EntryNotification(savedMate, fcmTopic);
+        notificationService.saveAndScheduleNotification(entryNotification.toNotification());
+
+        // 약속 리마인더 알림 스케줄링
+        ReminderNotification reminderNotification = new ReminderNotification(meeting, savedMate, fcmTopic);
+        notificationService.saveAndScheduleNotification(reminderNotification.toNotification());
+    }
+
+    @Transactional
+    public void HurryUpMate(HurryUpRequest request) {
+        Mate sender = mateQueryService.findById(request.getSenderId());
+        Mate receiver = mateQueryService.findById(request.getReceiverId());
+        validateMateCondition(sender, receiver);
+
+        Meeting meeting = meetingQueryService.findById(sender.getMeeting().getId());
+        if (meeting.checkAnHourBeforeMeetingTime()) {
+            throw new BadRequestException(NOT_ONE_HOUR_BEFORE_MEETING_ERROR);
+        }
+
+        HurryUpNotification hurryUpNotification = new HurryUpNotification(receiver);
+        notificationService.sendHurryUpNotification(sender, hurryUpNotification);
+    }
+
+    private void validateMateCondition(Mate sender, Mate receiver) {
+        if (!sender.getMeeting().getId().equals(receiver.getMeeting().getId())) {
+            throw new BadRequestException(NOT_SAME_MEETING_ERROR);
+        }
+
+        if (!validateLateMate(receiver)) {
+            throw new BadRequestException(NOT_LATE_MATE_ERROR);
+        }
+    }
+
+    private boolean validateLateMate(Mate receiver) {
+        EtaStatus etaStatus = etaService.findEtaStatus(receiver);
+        return etaStatus == EtaStatus.LATE;
     }
 }
