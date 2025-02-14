a6c7cd2b1aca6bad467d71cd559a694860b7b64e
bs-koo <bs.koo@sqisoft.com>
2025-02-14 15:01:56 +0900

    feat: 참여자 재촉 API 추가 - #59

--- src/main/java/org/example/odiya/mate/controller/MateController.java
+++ src/main/java/org/example/odiya/mate/controller/MateController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.odiya.common.annotation.AuthMember;
+import org.example.odiya.mate.dto.request.HurryUpRequest;
 import org.example.odiya.mate.dto.request.MateJoinRequest;
 import org.example.odiya.mate.dto.response.MateJoinResponse;
 import org.example.odiya.mate.service.MateService;
@@ -32,5 +33,15 @@ public class MateController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(mateJoinResponse);
+    }
+
+    @Operation(summary = "참여자 재촉 API", description = "참여자가 약속 참여자들을 재촉합니다.")
+    @PostMapping("/hurry-up")
+    public ResponseEntity<Void> hurryUp(@AuthMember Member member,
+                                        @Valid @RequestBody HurryUpRequest request) {
+        mateService.hurryUpMate(request);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
     }
 }
