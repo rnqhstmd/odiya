02f2bac631d2e0b13767098729ee2ffbdd6ae423
bs-koo <bs.koo@sqisoft.com>
2025-02-14 14:23:40 +0900

    refactor: 사용하지 않는 임포트 제거 - #57

--- src/test/java/org/example/odiya/notification/service/fcm/FcmPushSenderTest.java
+++ src/test/java/org/example/odiya/notification/service/fcm/FcmPushSenderTest.java
@@ -19,13 +19,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @Slf4j
