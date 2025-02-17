d9a6f688e4dc09684d8f05a5f6fc849c770866ca
bs-koo <bs.koo@sqisoft.com>
2025-02-17 17:54:35 +0900

    rename: KakaoPlaceSearchClient 로 이름 변경 - #20

--- src/main/java/org/example/odiya/place/controller/PlaceController.java
+++ src/main/java/org/example/odiya/place/controller/PlaceController.java
@@ -3,8 +3,8 @@ package org.example.odiya.place.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.example.odiya.place.dto.response.MapSearchResponse;
-import org.example.odiya.place.service.PlaceService;
+import org.example.odiya.place.dto.response.PlaceSearchResponse;
+import org.example.odiya.place.service.KakaoPlaceSearchClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +14,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class PlaceController {
 
-    private final PlaceService placeService;
+    private final KakaoPlaceSearchClient kakaoPlaceSearchClient;
 
     @Operation(summary = "장소 전체 조회 API", description = "사용자가 키워드로 장소를 검색합니다.")
     @GetMapping("/places")
-    public ResponseEntity<MapSearchResponse> searchLocation(@RequestParam String query) {
-        return ResponseEntity.ok(placeService.searchByKeyword(query));
+    public ResponseEntity<PlaceSearchResponse> searchLocation(@RequestParam String query) {
+        return ResponseEntity.ok(kakaoPlaceSearchClient.searchByKeyword(query));
     }
 }
