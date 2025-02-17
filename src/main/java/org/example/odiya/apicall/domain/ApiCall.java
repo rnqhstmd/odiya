b2d2a7ec31c952f9ee68ff9641445af1a94e5436
bs-koo <bs.koo@sqisoft.com>
2025-02-17 17:47:24 +0900

    feat: 외부 API 호출 통제용 엔티티 ApiCall 추가 - #62

---
+++ src/main/java/org/example/odiya/apicall/domain/ApiCall.java
@@ -0,0 +1,44 @@
+package org.example.odiya.apicall.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.odiya.common.domain.BaseEntity;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApiCall extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ClientType clientType;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column
+    private Integer count;
+
+    @Column
+    private Boolean enabled = true;
+
+    public ApiCall(ClientType clientType, Integer count, LocalDate date) {
+        this(null, clientType, date, count, false);
+    }
+
+    public void increaseCount() {
+        count++;
+    }
+
+    public void markAsDisabled() {
+        this.enabled = false;
+    }
+}
