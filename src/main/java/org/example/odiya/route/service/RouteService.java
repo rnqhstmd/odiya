6ef2302f1dddedf0981fae515dbdc9f73692307f
bs-koo <bs.koo@sqisoft.com>
2025-02-17 17:56:00 +0900

    feat: 경로계산 시 클라이언트별 ApiCall 호출 횟수 검증 및 count 기능 추가 - #62

--- src/main/java/org/example/odiya/route/service/RouteService.java
+++ src/main/java/org/example/odiya/route/service/RouteService.java
@@ -1,11 +1,15 @@
 package org.example.odiya.route.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.odiya.apicall.domain.ClientType;
+import org.example.odiya.apicall.service.ApiCallService;
+import org.example.odiya.common.exception.BadRequestException;
 import org.example.odiya.meeting.domain.Coordinates;
 import org.example.odiya.route.domain.RouteInfo;
 import org.springframework.stereotype.Service;
 
 import static org.example.odiya.common.constant.Constants.WALKING_THRESHOLD;
+import static org.example.odiya.common.exception.type.ErrorType.TOO_MANY_REQUEST_ERROR;
 
 @Service
 @RequiredArgsConstructor
@@ -13,15 +17,33 @@ public class RouteService {
 
     private final GoogleRouteClient googleRouteClient;
     private final TmapRouteClient tmapRouteClient;
+    private final ApiCallService apiCallService;
 
     public long calculateOptimalRoute(Coordinates origin, Coordinates target) {
+        validateBothClientsAvailable();
+
+        apiCallService.validateApiCallAvailable(ClientType.GOOGLE);
         RouteInfo transitInfo = googleRouteClient.calculateRouteTime(origin, target);
+        apiCallService.increaseCountByClientType(ClientType.GOOGLE);
 
         if (transitInfo.getDistance() <= WALKING_THRESHOLD) {
+            apiCallService.validateApiCallAvailable(ClientType.TMAP);
             RouteInfo walkingInfo = tmapRouteClient.calculateRouteTime(origin, target);
+            apiCallService.increaseCountByClientType(ClientType.TMAP);
+
             return Math.min(transitInfo.getMinutes(), walkingInfo.getMinutes());
         }
 
         return transitInfo.getMinutes();
+    }
+
+    private void validateBothClientsAvailable() {
+        boolean googleEnabled = apiCallService.getEnabledByClientType(ClientType.GOOGLE);
+        boolean tmapEnabled = apiCallService.getEnabledByClientType(ClientType.TMAP);
+
+        if (!googleEnabled || !tmapEnabled) {
+            throw new BadRequestException(TOO_MANY_REQUEST_ERROR,
+                    "일부 경로 계산 서비스가 사용 불가능한 상태입니다.");
+        }
     }
 }
