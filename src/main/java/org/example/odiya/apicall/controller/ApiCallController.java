b5bb52eff9ec0d24aec457634c4097684f462c5c
bs-koo <bs.koo@sqisoft.com>
2025-02-18 14:38:26 +0900

    feat: Tag 추가 - #62

--- src/main/java/org/example/odiya/apicall/controller/ApiCallController.java
+++ src/main/java/org/example/odiya/apicall/controller/ApiCallController.java
@@ -1,6 +1,7 @@
 package org.example.odiya.apicall.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.odiya.apicall.dto.response.ApiCallCountResponse;
 import org.example.odiya.apicall.service.ApiCallService;
@@ -8,16 +9,19 @@ import org.example.odiya.apicall.util.ClientTypeMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "ApiCall API", description = "외부 API 호출 관련 API")
 @RestController
+@RequestMapping("/admin/api-call")
 @RequiredArgsConstructor
 public class ApiCallController {
 
     private final ApiCallService apiCallService;
 
     @Operation(summary = "API 호출 횟수 조회 API", description = "특정 클라이언트의 API 호출 횟수를 조회합니다.")
-    @GetMapping("/admin/api-call/count/{clientName}")
+    @GetMapping("/count/{clientName}")
     public ResponseEntity<ApiCallCountResponse> countApiCall(@PathVariable String clientName) {
         ApiCallCountResponse apiCallCountResponse = apiCallService.returnApiCountResponse(ClientTypeMapper.from(clientName));
         return ResponseEntity.ok(apiCallCountResponse);
