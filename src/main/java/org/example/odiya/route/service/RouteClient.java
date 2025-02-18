d07a7a9c1e723e175d54d071fc5adc54c1719fec
bs-koo <bs.koo@sqisoft.com>
2025-02-18 13:49:36 +0900

    feat: ApiClient 인터페이스 상속하도록 변경 - #64

--- src/main/java/org/example/odiya/route/service/RouteClient.java
+++ src/main/java/org/example/odiya/route/service/RouteClient.java
@@ -1,12 +1,10 @@
 package org.example.odiya.route.service;
 
+import org.example.odiya.apicall.service.ApiClient;
 import org.example.odiya.meeting.domain.Coordinates;
-import org.example.odiya.apicall.domain.ClientType;
 import org.example.odiya.route.domain.RouteInfo;
 
-public interface RouteClient {
+public interface RouteClient extends ApiClient {
 
     RouteInfo calculateRouteTime(Coordinates origin, Coordinates target);
-
-    ClientType getClientType();
 }
