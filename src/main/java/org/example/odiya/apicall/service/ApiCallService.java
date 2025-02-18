755a64fedd6579678f3b2189dd90811053e258bc
bs-koo <bs.koo@sqisoft.com>
2025-02-18 12:58:37 +0900

    feat: 클라이언트타입 호출 한도 검증 메서드 추가 - #62

--- src/main/java/org/example/odiya/apicall/service/ApiCallService.java
+++ src/main/java/org/example/odiya/apicall/service/ApiCallService.java
@@ -7,12 +7,15 @@ import org.example.odiya.apicall.domain.ClientType;
 import org.example.odiya.apicall.dto.response.ApiCallCountResponse;
 import org.example.odiya.apicall.repository.ApiCallRepository;
 import org.example.odiya.common.exception.BadRequestException;
+import org.example.odiya.common.exception.InternalServerException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.example.odiya.common.exception.type.ErrorType.API_CALL_DISABLED_ERROR;
 import static org.example.odiya.common.exception.type.ErrorType.TOO_MANY_REQUEST_ERROR;
 
 @Slf4j
@@ -44,9 +47,14 @@ public class ApiCallService {
     }
 
     @Transactional
-    public void increaseCountByClientType(ClientType clientType) {
+    public void recordApiCall(ClientType clientType) {
         ApiCall apiCall = findOrSaveTodayApiCallByClientType(clientType);
         apiCall.increaseCount();
+
+        int totalCount = countTotalApiCount(clientType);
+        if (totalCount >= clientType.getMonthlyLimit()) {
+            apiCall.markAsDisabled();
+        }
     }
 
     public ApiCall findOrSaveTodayApiCallByClientType(ClientType clientType) {
@@ -58,20 +66,26 @@ public class ApiCallService {
                 });
     }
 
-    public void validateApiCallAvailable(ClientType clientType) {
-        ApiCall apiCall = findOrSaveTodayApiCallByClientType(clientType);
-        int totalCount = countTotalApiCount(clientType);
-        if (totalCount >= clientType.getDailyLimit()) {
-            apiCall.markAsDisabled();
-            log.error("API 호출 한도 초과 - clientType: {}, 현재 호출 수: {}, 일일 제한: {}",
-                    clientType, totalCount, clientType.getDailyLimit());
-            throw new BadRequestException(TOO_MANY_REQUEST_ERROR,
-                    String.format("일일 호출 한도를 초과한 API : %s", clientType.name()));
-        }
-    }
+    public void validateClientsAvailable() {
+        Arrays.stream(ClientType.values())
+                .filter(clientType ->
+                                clientType == ClientType.GOOGLE ||
+                                clientType == ClientType.TMAP ||
+                                clientType == ClientType.KAKAO)
+                .forEach(clientType -> {
+                    ApiCall apiCall = findOrSaveTodayApiCallByClientType(clientType);
 
-    public boolean getEnabledByClientType(ClientType clientType) {
-        ApiCall apiCall = findOrSaveTodayApiCallByClientType(clientType);
-        return apiCall.getEnabled();
+                    if (Boolean.FALSE.equals(apiCall.getEnabled())) {
+                        throw new InternalServerException(API_CALL_DISABLED_ERROR,
+                                String.format("%s API가 비활성화 상태입니다.", clientType.name()));
+                    }
+
+                    int totalCount = countTotalApiCount(clientType);
+                    if (totalCount >= clientType.getMonthlyLimit()) {
+                        apiCall.markAsDisabled();
+                        throw new BadRequestException(TOO_MANY_REQUEST_ERROR,
+                                String.format("%s API 호출 한도를 초과했습니다.", clientType.name()));
+                    }
+                });
     }
 }
